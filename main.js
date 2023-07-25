c3da68dff6fb3ca158463f84617c77faf9f4e46f
baron <buaisaac17@gmail.com>
2023-07-25 11:57:40 +0300

    added event listeners to the buttons

--- main.js
+++ main.js
@@ -9,9 +9,53 @@ function createGrids(gridSize) {
 
     for (let i = 0; i < numGrid; i++){
         let gridDivs = document.createElement('div');
-        gridDivs.style.backgroundColor = 'blue';
+        gridDivs.addEventListener('mouseover', colorDiv);
         myBoard.insertAdjacentElement('beforeend', gridDivs);
     }
 };
 
-createGrids(32);
+//taking user input
+function userChoice(){
+    let choice = prompt("Input put your gridsize choice?");
+    if (choice === " "){
+        console.log('input your choice');
+    } else if (choice < 2 || choice > 100) {
+        console.log('Input a number between 2 and 100');
+    } else {
+        console.log('Start Drawing your sketch!!');
+        return choice;
+    };
+    
+};
+
+//select button
+let selectSize = document.querySelector('#selectSize');
+selectSize.addEventListener('click', function(){
+    let size = userChoice();
+    createGrids(size);
+});
+
+//erase function
+function eraseBoard(){
+    let clear = document.querySelectorAll('div');
+    clear.forEach((div) => div.style.backgroundColor = 'white'); 
+};
+let erase = document.querySelector('#erase');
+erase.addEventListener('click', function(){
+    eraseBoard();
+})
+//choosing colors for use
+let color = 'black';
+function colorDiv(){
+    if (color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = 'black';
+    };
+};
+
+function setColor(colorChoice){
+    color = colorChoice;
+};
+
+
